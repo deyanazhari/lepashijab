7550e9409fad8ce5e58ae9d17fd0ca4bb12697b8
deyanazhari <deyanazhari@gmail.com>
2021-11-15 17:55:40 +0700

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# lepas_hijab
--- index.php
+++ index.php
@@ -97,7 +97,7 @@ $koneksi = new mysqli ("localhost","root","","lepashijab");
             </div>
             <div class="banner-item-03">
                 <div class="text-content">
-                    <h4>Last Minute</h4>
+                    <h4></h4>
                     <h2></h2>
                 </div>
             </div>
