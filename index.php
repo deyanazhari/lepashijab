afbdc07ba0209fe61a09569a99d645a726bdbb7c
deyanazhari <deyanazhari@gmail.com>
2021-11-15 18:34:14 +0700

    ayooo

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ $koneksi = new mysqli ("localhost","root","","lepashijab");
             </div>
             <div class="banner-item-03" style="background-image: url(assets/images/header3.png);">
                 <div class="text-content">
-                    <h4></h4>
+                    <h4>Ayoo beli</h4>
                     <h2></h2>
                 </div>
             </div>
