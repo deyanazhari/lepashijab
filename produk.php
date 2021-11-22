cbb7723f7d9a2d32376dfd27a70c03ce46abcaaa
deyanazhari <deyanazhari@gmail.com>
2021-11-22 22:26:02 +0700

    bosen

--- produk.php
+++ produk.php
@@ -86,13 +86,14 @@ $koneksi = new mysqli ("localhost","root","","lepashijab");
     </header>
 
     <!-- Page Content -->
-    <div class="page-heading products-heading header-text">
+    <div class="page-heading products-heading header-text"
+        style="background-image:url(assets/images/produk-bg.jpeg);padding: 300px 0px;">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="text-content">
                         <h4>new arrivals</h4>
-                        <h2>Lepas Hijab produk</h2>
+                        <h2 style="font-size: 30px;">Lepas Hijab produk</h2>
                     </div>
                 </div>
             </div>
